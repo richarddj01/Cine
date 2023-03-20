3ad4d959e065ecd3c55a6af6861d93bd9cc1031c
Joel Humberto <joelhumberto334@gmail.com>
2023-03-20 00:06:42 -0600

    actualizacion incompleta

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.galery').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        centerMode: true,
+        variableWidth: true
+      });
+  });
+          
