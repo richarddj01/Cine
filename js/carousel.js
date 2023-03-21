7a318272193f4252dd75662ea8bb8610328ed6d2
Joel Humberto <joelhumberto334@gmail.com>
2023-03-20 20:56:46 -0600

    cambios

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,40 @@
+$(document).ready(function () {
+  let $carousel = $(".galery");
+  let InicializadorSlick = false;
 
-$(document).ready(function(){
-    $('.galery').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
+  function initSlick() {
+    if ($(window).width() <= 960 && !InicializadorSlick) {
+      $carousel.slick({
         centerMode: true,
-        variableWidth: true
+        centerPadding: "60px",
+        slidesToShow: 2,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: "40px",
+              slidesToShow: 1,
+            },
+          },
+        ],
       });
-  });
-          
+      InicializadorSlick = true;
+    } else if ($(window).width() > 960 && InicializadorSlick) {
+      $carousel.slick("unslick");
+      InicializadorSlick = false;
+    }
+  }
+  initSlick();
+  $(window).resize(initSlick);
+});
