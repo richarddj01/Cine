88cbe2f2d9459974fc6f7c5d3a8340162a13014d
JoseFortin <99062801+JoseFortin@users.noreply.github.com>
2023-04-15 22:46:59 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,26 +1,38 @@
+jose fortin
 <?php
-
 include('conexion.php');
 
-// Agarra los datos del Input
+// Agarrar los datos del Input
 $nombre = $_POST['nombre'];
 $correo = $_POST['emai2'];
 $contrasena = $_POST['contrasen'];
 $telefono = $_POST['telefon'];
 
-
-// Insertar los datos en la tabla "usuarios"
-$sql = "INSERT INTO usuarios (nombre, correo, contrasena, telefono) VALUES (?, ?, ?, ?)";
+// Verificar si el correo electrónico ya existe en la base de datos
+$sql = "SELECT correo FROM usuarios WHERE correo = ?";
 $stmt = mysqli_prepare($conexion, $sql);
-mysqli_stmt_bind_param($stmt, "ssss", $nombre, $correo, $contrasena, $telefono);
+mysqli_stmt_bind_param($stmt, "s", $correo);
+mysqli_stmt_execute($stmt);
+$resultado = mysqli_stmt_get_result($stmt);
 
-if (mysqli_stmt_execute($stmt)) {
-  header("Location: index.html");
-  exit();
+if (mysqli_num_rows($resultado) > 0) {
+  // El correo electrónico ya existe en la base de datos, mostrar mensaje de error
+  echo "El correo electrónico ingresado ya existe.";
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+  // El correo electrónico no existe en la base de datos, insertar los datos en la tabla "usuarios"
+  $sql = "INSERT INTO usuarios (nombre, correo, contrasena, telefono) VALUES (?, ?, ?, ?)";
+  $stmt = mysqli_prepare($conexion, $sql);
+  mysqli_stmt_bind_param($stmt, "ssss", $nombre, $correo, $contrasena, $telefono);
+
+  if (mysqli_stmt_execute($stmt)) {
+    header("Location: index.html");
+    exit();
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+  }
+
+  mysqli_stmt_close($stmt);
 }
 
-mysqli_stmt_close($stmt);
 mysqli_close($conexion);
 ?>
